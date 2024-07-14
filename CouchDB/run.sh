71fea982c352ce179e3a8cf72e41f521d40fbdd4
Madoka2000 <72033989+Madoka2000@users.noreply.github.com>
2024-07-14 19:31:16 +0200

    Add files via upload

--- CouchDB/run.sh
+++ CouchDB/run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-# Démarrer CouchDB
+# Lire les variables d'environnement
+COUCHDB_USER=${COUCHDB_USER:-admin}
+COUCHDB_PASSWORD=${COUCHDB_PASSWORD:-password}
+
+# Démarrer CouchDB avec des informations d'identification d'administrateur
+export COUCHDB_USER COUCHDB_PASSWORD
 couchdb
